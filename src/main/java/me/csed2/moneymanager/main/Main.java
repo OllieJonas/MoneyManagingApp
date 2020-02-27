99a7ac34d9a4ba9a2d1ca002f87cb940fc56ac29
Ollie <olliejonas@gmail.com>
2020-02-27 13:55:09 +0000

    Implemented JUnit

---
+++ src/main/java/me/csed2/moneymanager/main/Main.java
@@ -0,0 +1,22 @@
+package me.csed2.moneymanager.main;
+
+public class Main {
+
+    public Main() {
+        System.out.println("Hello world!");
+    }
+
+    /**
+     * Method that multiplies 2 numbers together, used for testing JUnit
+     * @param a First number
+     * @param b Second number
+     * @return a*b
+     */
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
---
+++ src/test/java/me/csed2/moneymanager/TestMoneyManager.java
@@ -0,0 +1,19 @@
+package me.csed2.moneymanager;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import me.csed2.moneymanager.main.Main;
+
+public class TestMoneyManager {
+
+    /**
+     * Testing if JUnit works
+     */
+    @Test
+    public void helloWorldTest() {
+        Main main = new Main();
+        assertEquals(main.multiply(3,2), 6);
+        // assertEquals(main.multiply(3,2), 7);
+    }
+}
