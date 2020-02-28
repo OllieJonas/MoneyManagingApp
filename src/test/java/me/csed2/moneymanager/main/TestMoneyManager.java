8ed9b5c10472aa640f20ee6d7f6cafb4d628a3ca
Ollie <olliejonas@gmail.com>
2020-02-28 15:17:50 +0000

    Tidying up of package structure. Added lombok and guava as dependencies.

--- src/test/java/me/csed2/moneymanager/main/TestMoneyManager.java
+++ src/test/java/me/csed2/moneymanager/main/TestMoneyManager.java
@@ -3,8 +3,6 @@ package me.csed2.moneymanager.main;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import me.csed2.moneymanager.main.Main;
-
 public class TestMoneyManager {
 
     /**
@@ -13,7 +11,7 @@ public class TestMoneyManager {
     @Test
     public void helloWorldTest() {
         Main main = new Main();
-        assertEquals(main.multiply(3,2), 6);
+        assertEquals(main.multiply(3, 2), 6);
         // assertEquals(main.multiply(3,2), 7);
     }
 }
