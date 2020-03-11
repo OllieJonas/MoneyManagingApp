27d1aa76d8281860d4bdfb800a15fefed10b1168
jacg <jjg62@bath.ac.uk>
2020-03-11 13:14:46 +0000

    Got a working blank JFrame.

--- src/main/java/me/csed2/moneymanager/ui/gui/Display.java
+++ src/main/java/me/csed2/moneymanager/ui/gui/Display.java
@@ -1,6 +1,7 @@
 package me.csed2.moneymanager.ui.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Display {
 
@@ -11,19 +12,29 @@ public class Display {
     private int width, height;
     private String title;
 
-    public Display(){
+    public Display(int width, int height, String title){
+        this.width = width;
+        this.height = height;
+        this.title = title;
 
+        initDisplay();
     }
 
     /**
      * Initiate and place widgets on frame
      */
     private void initDisplay(){
-
+        initFrame();
     }
 
     private void initFrame(){
+        frame = new JFrame(title);
 
+        frame.setSize(new Dimension(width, height));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exit program on close
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null); //Center on screen
+        frame.setVisible(true);
     }
 
 }
