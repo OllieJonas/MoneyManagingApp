15c21df8bbd94a7e6e9fa52962f0a261e9d9a692
jacg <jjg62@bath.ac.uk>
2020-03-11 13:08:07 +0000

    Started new GUI Branch. Working on Display Class

---
+++ src/main/java/me/csed2/moneymanager/ui/gui/Display.java
@@ -0,0 +1,29 @@
+package me.csed2.moneymanager.ui.gui;
+
+import javax.swing.*;
+
+public class Display {
+
+    //Frame
+    private JFrame frame;
+
+    //Frame information
+    private int width, height;
+    private String title;
+
+    public Display(){
+
+    }
+
+    /**
+     * Initiate and place widgets on frame
+     */
+    private void initDisplay(){
+
+    }
+
+    private void initFrame(){
+
+    }
+
+}
