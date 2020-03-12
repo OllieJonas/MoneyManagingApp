abeda44a9a3a420143ef82be51c841ede7f524eb
jacg <jjg62@bath.ac.uk>
2020-03-12 13:35:23 +0000

    Restructured the Display classes. Started on DisplayStageMenu.

--- src/main/java/me/csed2/moneymanager/ui/gui/DisplayButtonMenu.java
+++ src/main/java/me/csed2/moneymanager/ui/gui/DisplayButtonMenu.java
@@ -1,73 +1,29 @@
 package me.csed2.moneymanager.ui.gui;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Map;
 
 import me.csed2.moneymanager.ui.Menu;
 import me.csed2.moneymanager.ui.Button;
 
-public abstract class Display implements Menu{
-
-    //Create Static Instance for each Menu
-    public static final Display DISPLAY_EXAMPLE = new DisplayExample();
-
-    //Frame
-    protected JFrame frame;
-    protected JPanel panel;
-
-    //Frame information
-    private int width, height;
-    private String title;
+public abstract class DisplayButtonMenu extends DisplayMenu {
 
     //Buttons
     private ArrayList<Button> buttons;
 
-    public Display(int width, int height, String title){
+    public DisplayButtonMenu(int width, int height, String title) {
         this.width = width;
         this.height = height;
         this.title = title;
 
         initFrame(); //Create the Frame and Panel
+        buttons = new ArrayList<Button>();
         addButtons(); //Called by Subclasses - add all buttons
         placeButtons(); //Turn those buttons into JButtons
 
-
         frame.pack();
     }
-
-    private void initFrame(){
-        frame = new JFrame(title);
-
-        frame.setSize(new Dimension(width, height));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exit program on close
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null); //Center on screen
-        frame.setVisible(false);
-
-        panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-        frame.add(panel);
-
-        addTitle(title);
-        buttons = new ArrayList<Button>();
-    }
-
-    /** Place a Title Label for this menu.
-     * @param title The string to display as the title.
-     */
-    protected void addTitle(String title){
-        JLabel labTitle = new JLabel(title);
-        labTitle.setAlignmentX(Component.CENTER_ALIGNMENT); //Place title in the middle
-        labTitle.setFont(new Font("TimesRoman", Font.BOLD, 20));
-        panel.add(labTitle);
-    }
-
     protected abstract void addButtons();
 
     /**
