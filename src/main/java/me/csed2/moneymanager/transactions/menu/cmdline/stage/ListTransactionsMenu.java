97c85e6daa6088b86df5a160a653c69ef4338671
Ollie <olliejonas@gmail.com>
2020-03-18 13:33:30 +0000

    Abstracted Listing Transaction Command
    Small fix on adding transactions.

--- src/main/java/me/csed2/moneymanager/transactions/menu/cmdline/stage/ListTransactionsMenu.java
+++ src/main/java/me/csed2/moneymanager/transactions/menu/cmdline/stage/ListTransactionsMenu.java
@@ -1,10 +1,8 @@
 package me.csed2.moneymanager.transactions.menu.cmdline.stage;
 
-import me.csed2.moneymanager.categories.Category;
-import me.csed2.moneymanager.categories.CategoryCache;
 import me.csed2.moneymanager.command.CommandDispatcher;
 import me.csed2.moneymanager.transactions.Transaction;
-import me.csed2.moneymanager.transactions.TransactionCache;
+import me.csed2.moneymanager.transactions.commands.ListTransactionsCommand;
 import me.csed2.moneymanager.ui.Menu;
 import me.csed2.moneymanager.ui.cmdline.stage.Stage;
 import me.csed2.moneymanager.ui.cmdline.stage.StageMenu;
@@ -12,6 +10,7 @@ import me.csed2.moneymanager.ui.cmdline.stage.StageMenu;
 import java.util.List;
 
 public class ListTransactionsMenu extends StageMenu {
+
     /**
      * Constructor for CMDMenu. Responsible for adding the buttons to the list.
      *
@@ -28,33 +27,13 @@ public class ListTransactionsMenu extends StageMenu {
 
     @Override
     public void exitPhase() {
-        CategoryCache repository = CategoryCache.getInstance();
         String result = (String) stages.get(0).getResult();
+        List<Transaction> transactions = CommandDispatcher.getInstance().dispatchSync(new ListTransactionsCommand(result));
 
-        if (result.equalsIgnoreCase("ALL")) {
-            for (Transaction transaction : TransactionCache.getInstance().asList()) {
-                System.out.println(transaction.toFormattedString());
-                openPreviousMenu();
-            }
-        } else {
-            Category category = repository.readByName(result);
-
-            if (category != null) {
-                try {
-
-                    List<Transaction> transactions = TransactionCache.getInstance().readByCategory(result);
-
-                    for (Transaction transaction : transactions) {
-                        System.out.println(transaction.toFormattedString());
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                openPreviousMenu();
-            } else {
-                System.out.println("Error: Unable to find this category!");
-                restart();
-            }
+        for (Transaction transaction : transactions) {
+            System.out.println(transaction.toFormattedString());
         }
+
+        openPreviousMenu();
     }
 }
